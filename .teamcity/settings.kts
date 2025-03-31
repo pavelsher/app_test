89aada38355f3304d9a6edcd4a78242e0b6ee6e1
admin <admin@TeamCity>
2025-03-31 13:02:31 +0200

    general settings of 'Build All' build configuration were updated (TeamCity change in 'Mass triggering' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -38,7 +38,7 @@ project {
 
 object BuildAll : BuildType({
     name = "Build All"
-    description = "builds everything 1234567890123"
+    description = "builds everything 123456789012345"
 
     type = BuildTypeSettings.Type.COMPOSITE
 
